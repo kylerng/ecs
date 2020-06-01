35ffe02e4fd0f8fa52431a2421b81e119f7c201b
supermng <supermng@users.noreply.github.com>
2020-06-01 18:14:21 +0800

    add world

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,8 @@ export * from './util/unique';
 export * from './util/type.utils';
 export * from './util/event-source';
 
+// world
+export * from './world/World';
+export * from './world/System';
+export * from './world/Component';
+export * from './world/Entity';
